63e0ae5d4d9be42ea758788e9714e511f87017eb
Skyler Mishkin <meestermishkin@gmail.com>
2018-08-26 16:33:25 -0600

    adding post view and template; need to troubleshoot why the landing's custom css won't load

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render, get_object_or_404
 def index(request):
     context = {}
     return render(request, 'blog/index.html', context)
+
+
+def post(request):
+    context = {}
+    return render(request, 'blog/post.html', context)
