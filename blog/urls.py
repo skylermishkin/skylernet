280db69c402ccec5156ea11cf6324f39654460a0
Skyler Mishkin <meestermishkin@gmail.com>
2018-08-27 11:28:09 -0600

    nav urls work with django template tags

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/post_test', views.post, name='post_test')
+    path('post_test', views.post, name='post_test'),
 ]
--- skylernet/urls.py
+++ skylernet/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from . import views
 
 
+app_name = 'skylernet'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
