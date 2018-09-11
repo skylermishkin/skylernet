0bb9f7554efaebdfcbc60da1fc08a39fc7c2c044
Skyler Mishkin <meestermishkin@gmail.com>
2018-09-11 17:57:30 -0600

    just starting/learning blog models and media

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=40)
+
+
 class Post(models.Model):
-    title = models.TextField(max_length=100)
+    title = models.CharField(primary_key=True, unique=True, max_length=100)
+    author = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(auto_now=False, auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     image = models.ImageField()
-    author = models.TextField(max_length=20)
-    date_posted = models.DateTimeField(auto_now=True)
     content = models.TextField(max_length=10000)
+    briefing = models.TextField(max_length=1000)
+    tags = models.ManyToManyField(Tag)
--- skylernet/settings.py
+++ skylernet/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
