e1b6c6f31ace708e33f0b0572d4dbbfb64c34edd
Skyler Mishkin <meestermishkin@gmail.com>
2018-09-15 12:48:21 -0600

    spiffing up some templates; adding a bit more content

--- skylernet/views.py
+++ skylernet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
 def index(request):
@@ -6,5 +7,10 @@ def index(request):
 
 
 def connect(request):
-    context = {'social_media': []}
+    context = {'online_media': [{"name": 'LinkedIn',
+                                 'href': 'https://www.linkedin.com/in/skyler-mishkin-62446b158',
+                                 'src': static('skylernet/LinkedIn.svg')},
+                                {'name': 'GitHub',
+                                 'href': 'https://github.com/skylermishkin',
+                                 'src': static('skylernet/GitHub.png')}]}
     return render(request, 'skylernet/connect.html', context)
